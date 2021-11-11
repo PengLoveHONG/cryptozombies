21d8beeeae80b8125ecf180614544d7f435da49c
leovct <l009.vincent@gmail.com>
2021-11-11 18:41:54 +0100

    create zombie-factory contract

---
+++ zombie-factory.sol
@@ -0,0 +1,9 @@
+// Define the license of the code
+// SPDX-License-Identifier: UNLICENSED
+
+// Define the version of the Solidity compiler to use
+pragma solidity >=0.8.10;
+
+contract ZombieFactory {
+    // TODO: To implement
+}
