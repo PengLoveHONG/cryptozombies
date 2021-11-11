20c3784f482b64dca255c26cf8a92f8e6df7d280
leovct <l009.vincent@gmail.com>
2021-11-11 18:50:13 +0100

    add state variable

--- zombie-factory.sol
+++ zombie-factory.sol
@@ -5,5 +5,9 @@
 pragma solidity >=0.8.10;
 
 contract ZombieFactory {
-    // TODO: To implement
+    // State variable that will be stored permanently in the Ethereum blockchain.
+    // The uint type corresponds to an unsigned integer, meaning its value must be non-negative.
+    // Its size is 256 bits but it's possible to declare uints with less bits (uint8, unit16...).
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
 }
