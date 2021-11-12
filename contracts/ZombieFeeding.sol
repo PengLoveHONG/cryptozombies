b0bb56674f606b85e5a7fb3d187137457327a0d2
leovct <l009.vincent@gmail.com>
2021-11-12 14:17:41 +0100

    transform zombies that feed on kitties into cat zombies

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -37,11 +37,18 @@ contract ZombieFeeding is ZombieFactory {
     // permanently to the blockchain, while variables declared inside functions are memory and will
     // disappear when the function call ends. However, there are times when we do need to use these
     // keywords, namely when dealing with structs and arrays within functions.
-    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        // If the species used to feed the zombie is a kitty, transform the zombie into a cat
+        // zombie. It is done by replacing the last two digits of the dna by 99.
+        // ie. newDna is 334455, newDna % 100 is 55 so newDna - newDna % 100 is 334400
+        // then, if we add 99, the new Dna value is 334499
+        if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+            newDna = newDna - newDna % 100 + 99;
+        }
         _createZombie("NoName", newDna);
     }
 
@@ -50,6 +57,6 @@ contract ZombieFeeding is ZombieFactory {
         // Since the getKitty function returns 10 variables, we can either use all or some of them.
         // Writing a comma with no variable name means that we don't want to use the returned value.
         (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
