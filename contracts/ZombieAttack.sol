a6d0c408259b954edb288f58bdcd87ab95d2eb27
leovct <l009.vincent@gmail.com>
2021-11-12 17:17:11 +0100

    add a function to generate a semi-random number

--- contracts/ZombieAttack.sol
+++ contracts/ZombieAttack.sol
@@ -4,5 +4,15 @@ pragma solidity ^0.8.0;
 import "./ZombieHelper.sol";
 
 contract ZombieAttack is ZombieHelper {
+    uint256 randNonce = 0;
 
+    // This function generate a semi-random number but it's not a secure way to do it at all!!
+    // It's only for learning purposes but we should prefer using oracle instead of keccak256 to
+    // generate random numbers. Why? This is subject to an attack by a dishonest node.
+    function randMod(uint256 _modulus) internal returns (uint256) {
+        randNonce++;
+        return
+            uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) %
+            _modulus;
+    }
 }
